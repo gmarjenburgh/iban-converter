5f305772fb8a6eb14bf06d138517ed07d3017009
Giovanni Marjenburgh <giovanni.marjenburgh@gmail.com>
2016-01-11 20:24:55 +0100

    Class IBAN created

---
+++ src/main/java/nl/booyah/iban/IBAN.java
@@ -0,0 +1,8 @@
+package nl.booyah.iban;
+
+public class IBAN {
+	private String countryCode;
+	private int checkSum;
+	private String bankCode;
+	private String accountNumber; // Always 10 characters
+}
