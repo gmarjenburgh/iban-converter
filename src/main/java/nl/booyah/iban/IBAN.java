db45d591376e6200b52caa9870379d60f8946304
Giovanni Marjenburgh <giovanni.marjenburgh@gmail.com>
2016-01-11 20:52:28 +0100

    created toString method IBAN class and IBAN constructor

--- src/main/java/nl/booyah/iban/IBAN.java
+++ src/main/java/nl/booyah/iban/IBAN.java
@@ -2,7 +2,23 @@ package nl.booyah.iban;
 
 public class IBAN {
 	private String countryCode;
-	private int checkSum;
+	private String checkDigits;
 	private String bankCode;
 	private String accountNumber; // Always 10 characters
+	
+	@Override
+	public String toString(){
+		return countryCode + checkDigits + bankCode + accountNumber; 
+	}
+	
+	IBAN(String bankCode, String accountNumber){
+		this.countryCode = "NL";
+		this.checkDigits = checkDigits(bankCode,accountNumber);
+		this.bankCode = bankCode;		
+		this.accountNumber = "0000000000".substring(accountNumber.length()) + accountNumber;
+	}
+	
+	void printIBAN(){
+		System.out.println(countryCode+checkDigits+bankCode+accountNumber);
+	}
 }
--- src/main/java/nl/booyah/iban/Main.java
+++ src/main/java/nl/booyah/iban/Main.java
@@ -3,6 +3,6 @@ package nl.booyah.iban;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
 	}
 }
