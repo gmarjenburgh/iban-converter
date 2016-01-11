78e450a0082e1edc8e79f45b66578f976533c758
Daniel Marjenburgh <dmarjenburgh@gmail.com>
2016-01-11 21:35:42 +0100

    Implement command line interface. Runnable as jar

--- src/main/java/nl/booyah/iban/Main.java
+++ src/main/java/nl/booyah/iban/Main.java
@@ -3,6 +3,10 @@ package nl.booyah.iban;
 public class Main {
 
 	public static void main(String[] args) {
-			// TODO Auto-generated method stub
+		String bankCode = args[0];
+		String accountNumber = args[1];
+		IBAN iban = new IBAN(bankCode, accountNumber);
+		System.out.println(iban);
+		System.exit(0);
 	}
 }
