c3f1e74943343c726aa01fd9b7584122a02a7ab1
Giovanni Marjenburgh <giovanni.marjenburgh@gmail.com>
2016-01-11 16:06:32 +0100

    elfproef function added

--- src/main/java/nl/booyah/iban/Main.java
+++ src/main/java/nl/booyah/iban/Main.java
@@ -4,7 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
+	
+	public static boolean elevenTest(long aNumber){
+		
+		int lengte = String.valueOf(aNumber).length();
+		int totaal=0;
+		boolean result = String.valueOf(aNumber).length()<=11;
+				
+		for (int i=1;i<lengte+1;i++){
+			totaal += i * (aNumber % 10);
+			aNumber /=10;
+		}
+		
+		result = (totaal % 11)==0;
+				
+		return result;
+	}
+
 
 }
